586e2325e6192bedfc624d04439c2927d3f92655
laranhee <laranhee@gmail.com>
2019-10-27 15:56:31 +0900

    포스트 내 이미지 첨부 설정 추가, 포스트 추가(크롬에서 캐시 무시하기)

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,12 @@ module.exports = function(eleventyConfig) {
     'favicon.ico': 'favicon.ico'
   });
 
+  /**
+   * 포스트에 첨부되는 이미지들은 post-img 폴더에 포스트의 경로와 동일하게 위치시키며,
+   * 빌드 시 포스트들이 생성되는 동일한 위치로 복사한다.
+   */
+  eleventyConfig.addPassthroughCopy({ 'post-img': '.' });
+
   return {
     htmlTemplateEngine: 'njk'
   };
---
+++ post/chrome/disable-cache/index.md
@@ -0,0 +1,18 @@
+---
+title: 크롬에서 캐시 무시하기
+permalink: /chrome/disable-cache/index.html
+layout: post-layout.html
+tags: chrome
+---
+
+##### 크롬에서 캐시 무시하기
+
+###### 캐싱된 콘텐츠를 무시하고 새로고침
+
+Mac : cmd + shift + r
+
+###### 항상 캐싱하지 않기(개발자 도구가 열려있을 때)
+
+개발자 도구 -> Network 패널 -> Disable cache 체크
+
+![screenshot](01.png)
