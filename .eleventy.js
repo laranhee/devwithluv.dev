edf787d38e5ce79d14d47c178423fef69d2224f2
laranhee <laranhee@gmail.com>
2019-10-29 22:15:32 +0900

    소스 구조 변경 대응

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,7 @@ module.exports = function(eleventyConfig) {
    * 포스트에 첨부되는 이미지들은 post-img 폴더에 포스트의 경로와 동일하게 위치시키며,
    * 빌드 시 포스트들이 생성되는 동일한 위치로 복사한다.
    */
-  eleventyConfig.addPassthroughCopy({ 'post-img': '.' });
+  eleventyConfig.addPassthroughCopy({ 'src/post-img': '.' });
 
   return {
     dir: {
